5d58ba623e11c15d486d3930f099fd0fc39640e8
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-09-14 02:13:03 +0530

    tut 1 push

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,13 +1,14 @@
-def factorial(x):
-    factor = 1
-    if x<0:
-     print("Fctorial does not exist")
-    elif x==0:
-     print("Factorial of 0 = 1")  
-    else:
-     for i in range (1,x+1):
-         factor = factor*i
-    
-    print("Factorial of " ,x ,"is" ,factor,)
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+def octact_identification(mod=5000):
+###Code
+
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
