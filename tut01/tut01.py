7777d8a598583dfb593bebf48e185259875be655
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-09-14 22:58:40 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -10,14 +10,13 @@ def octact_identification(mod=5000):
  b2 = []    # declaring list for difference 
  a2 = []    # declaring list for difference
  c2 = []    # declaring list for difference
- oct = []   # declaring list for octant storing
+ octants = []   # declaring list for octant storing
  with open('octant_input.csv', 'r') as file:    #opening input file.
   reader = csv.reader(file)
   t = 0
   for row in reader:
    if (t == 0):
     t = t+1 # continue only for t==0
-    
    else:
     d1.append(row[0])
     a1.append(row[1])
@@ -27,3 +26,56 @@ def octact_identification(mod=5000):
   a_mean_value=np.mean(a1, dtype=np.float64) # for mean of a1
   c_mean_value=np.mean(c1, dtype=np.float64) # for mean of c1
 
+ with open('octant_input.csv', 'r') as file: # reopening input file
+  reader = csv.reader(file)
+  k=0
+  for row in reader:
+   if (k==0):
+    k=k+1
+   else:
+    a2.append(float(row[1])-a_mean_value)
+    b2.append(float(row[2])-b_mean_value)
+    c2.append(float(row[3])-c_mean_value)	
+
+ with open('octant_input.csv', 'r') as file:
+  reader = csv.reader(file)
+  l=0           # counting the number of rows.
+  for row in reader:
+   l=l+1
+  rows=l        # rows are number of rows
+  rows=rows-1
+  print(rows)
+ with open('octant_input.csv', 'r') as file:    #opening file for octants..
+  reader = csv.reader(file)  
+ 	
+  for l in range(rows):     #finding the octants using loop
+   if (l<rows-1):
+    if ((a2[l]>=0) and (b2[l]>=0) and (c2[l]>=0 )):
+     octants.append(1)
+    elif((a2[l]>=0) and (b2[l]>=0) and (c2[l]<0 )):
+     octants.append(-1)
+    elif((a2[l]<0) and (b2[l]>=0) and (c2[l]>=0 )):
+     octants.append(2)
+    elif((a2[l]<0) and (b2[l]>=0) and (c2[l]<0 )):
+     octants.append(-2)
+    elif((a2[l]>=0) and (b2[l]<0) and (c2[l]>=0 )):
+     octants.append(4)
+    elif((a2[l]>=0) and (b2[l]<0) and (c2[l]<0 )):
+     octants.append(-4)    
+    elif((a2[l]<0) and (b2[l]<0) and (c2[l]>=0 )):
+     octants.append(3)
+    else:
+     octants.append(-3) 
+
+ with open('octant_input.csv', 'r') as file: # opening input file 
+  reader = csv.reader(file)                     #for counting overall octants...
+  l=0
+  ct1=0         # counting of octant 1
+  ct2=0         # counting of octant -1
+  ct3=0         # counting of octant 2
+  ct4=0         # counting of octant -2
+  ct5=0         # counting of octant 3
+  ct6=0         # counting of octant -3
+  ct7=0         # counting of octant 4
+  ct8=0         # counting of octant -4	 
+  
