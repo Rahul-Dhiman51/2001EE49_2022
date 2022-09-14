660980dc76219de1a6690070ca3c327b003b309e
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-09-14 22:44:46 +0530

    tut 1 updated

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,29 @@
 def octact_identification(mod=5000):
-###Code
+ import csv
+ import os
+ import numpy as np
+ os.system('cls')
+ d1 = []    # declaring new list
+ b1 = []    # declaring new list
+ a1 = []    # declaring new list
+ c1 = []    # declaring new list
+ b2 = []    # declaring list for difference 
+ a2 = []    # declaring list for difference
+ c2 = []    # declaring list for difference
+ oct = []   # declaring list for octant storing
+ with open('octant_input.csv', 'r') as file:    #opening input file.
+  reader = csv.reader(file)
+  t = 0
+  for row in reader:
+   if (t == 0):
+    t = t+1 # continue only for t==0
+    
+   else:
+    d1.append(row[0])
+    a1.append(row[1])
+    b1.append(row[2])
+    c1.append(row[3])
+  b_mean_value=np.mean(b1, dtype=np.float64) # for mean of b1
+  a_mean_value=np.mean(a1, dtype=np.float64) # for mean of a1
+  c_mean_value=np.mean(c1, dtype=np.float64) # for mean of c1
 
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-mod=5000
-octact_identification(mod)
