4f849f5d29d43550dd50e47e94e4a60922a573d8
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-10-11 23:16:06 +0530

    Update tut04.py

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -119,6 +119,299 @@ def octant_longest_subsequence_count_with_range():
 
 #  print(list1)
 
+      # using for loop to get the count and length of longest subsequence for octant -1
+ 
+ for i in range(0,num_1-1):
+  if octants[i]==-1 :
+   lss2=lss2+1
+   if i==num_1-2:
+    if lss2>lssl2:
+     lssl2=lss2
+     lss2=0
+     ct2=1
+    elif lssl2>lss2:
+     lss2=0
+    else:
+     lss2=0
+     ct2=ct2+1 
+  else:
+    if lss2>lssl2:
+     lssl2=lss2
+     lss2=0
+     ct2=1
+    elif lssl2>lss2:
+     lss2=0
+    else:
+     lss2=0
+     ct2=ct2+1
+
+ count1=0
+
+ list2=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==-1:
+   if count1==0: 
+    list_2=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl2:
+   list2.append(list_2); 
+   count1=0
+
+#  print(list2) 
+       
+
+      # using for loop to get the count and length of longest subsequence for octant 2
+
+ for i in range(0,num_1-1):
+  if octants[i]==2 :
+   lss3=lss3+1
+   if i==num_1-2:
+    if lss3>lssl3:
+     lssl3=lss3
+     lss3=0
+     ct3=1
+    elif lssl3>lss3:
+     lss3=0
+    else:
+     lss3=0
+     ct3=ct3+1 
+  else:
+    if lss3>lssl3:
+     lssl3=lss3
+     lss3=0
+     ct3=1
+    elif lssl3>lss3:
+     lss3=0
+    else:
+     lss3=0
+     ct3=ct3+1    
+ 
+ count1=0
+
+ list3=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==2:
+   if count1==0: 
+    list_3=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl3:
+   list3.append(list_3); 
+   count1=0
+
+#  print(list3)  
+ 
+      # using for loop to get the count and length of longest subsequence for octant -2
+ 
+ for i in range(0,num_1-1):
+  if octants[i]==-2 :
+   lss4=lss4+1
+   if i==num_1-2:
+    if lss4>lssl4:
+     lssl4=lss4
+     lss4=0
+     ct4=1
+    elif lssl4>lss4:
+     lss4=0
+    else:
+     lss4=0
+     ct4=ct4+1 
+  else:
+    if lss4>lssl4:
+     lssl4=lss4
+     lss4=0
+     ct4=1 
+    elif lssl4>lss4:
+     lss4=0
+    else:
+     lss4=0
+     ct4=ct4+1    
+
+ count1=0
+
+ list4=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==-2:
+   if count1==0: 
+    list_4=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl4:
+   list4.append(list_4); 
+   count1=0
+
+#  print(list4)  
+     
+      # using for loop to get the count and length of longest subsequence for octant 3
+
+ for i in range(0,num_1-1):
+  if octants[i]==3 :
+   lss5=lss5+1
+   if i==num_1-2:
+    if lss5>lssl5:
+     lssl5=lss5
+     lss5=0
+     ct5=1
+    elif lssl5>lss5:
+     lss5=0
+    else:
+     lss5=0
+     ct5=ct5+1 
+  else:
+    if lss5>lssl5:
+     lssl5=lss5
+     lss5=0
+     ct5=1
+    elif lssl5>lss5:
+     lss5=0
+    else:
+     lss5=0
+     ct5=ct5+1    
+ 
+ count1=0
+
+ list5=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==3:
+   if count1==0: 
+    list_5=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl5:
+   list5.append(list_5); 
+   count1=0
+
+#  print(list5)  
+ 
+        # using for loop to get the count and length of longest subsequence for octant -3
+
+ for i in range(0,num_1-1):
+  if octants[i]==-3 :
+   lss6=lss6+1
+   if i==num_1-2:
+    if lss6>lssl1:
+     lssl6=lss6
+     lss6=0
+     ct6=1
+    elif lssl6>lss6:
+     lss6=0
+    else:
+     lss6=0
+     ct6=ct6+1 
+  else:
+    if lss6>lssl6:
+     lssl6=lss6
+     lss6=0
+     ct6=1
+    elif lssl6>lss6:
+     lss6=0
+    else:
+     lss6=0
+     ct6=ct6+1    
+ 
+ count1=0
+
+ list6=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==-3:
+   if count1==0: 
+    list_6=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl6:
+   list6.append(list_6); 
+   count1=0
+
+#  print(list6)  
+ 
+
+        # using for loop to get the count and length of longest subsequence for octant 4
+ 
+ for i in range(0,num_1-1):
+  if octants[i]==4 :
+   lss7=lss7+1
+   if i==num_1-2:
+    if lss7>lssl7:
+     lssl7=lss7
+     lss7=0
+     ct7=1
+    elif lssl7>lss7:
+     lss7=0
+    else:
+     lss7=0
+     ct7=ct7+1 
+  else:
+    if lss7>lssl7:
+     lssl7=lss7
+     lss7=0
+     ct7=1
+    elif lssl7>lss7:
+     lss7=0
+    else:
+     lss7=0
+     ct7=ct7+1
+
+ count1=0
+
+ list7=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==4:
+   if count1==0: 
+    list_7=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl7:
+   list7.append(list_7); 
+   count1=0
+
+#  print(list7)  
+ 
+     # using for loop to get the count and length of longest subsequence for octant -4
+
+ for i in range(0,num_1-1):
+  if octants[i]==-4 :
+   lss8=lss8+1
+   if i==num_1-2:
+    if lss8>lssl1:
+     lssl8=lss8
+     lss8=0
+     ct8=1
+    elif lssl8>lss8:
+     lss8=0
+    else:
+     lss8=0
+     ct8=ct8+1 
+  else:
+    if lss8>lssl8:
+     lssl8=lss8
+     lss8=0
+     ct8=1
+    elif lssl8>lss8:
+     lss8=0
+    else:
+     lss8=0
+     ct8=ct8+1    
+       
+ count1=0
+
+ list8=[]
+ for i in range(0,num_1-1):  
+  if octants[i]==-4:
+   if count1==0: 
+    list_8=d1[i] 
+   count1=count1+1
+  else:
+   count1=0 
+  if count1==lssl8:
+   list8.append(list_8); 
+   count1=0
 
 
 
