68e68f4da6d8423294febcc4f4343e32d4ce0151
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-10-11 21:40:13 +0530

    Update tut04.py

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,22 +1,55 @@
-
-from datetime import datetime
-start_time = datetime.now()
-
-#Help https://youtu.be/H37f_x4wAC0
 def octant_longest_subsequence_count_with_range():
-###Code
+ from openpyxl import load_workbook
+ import os
+ import numpy as np
+ os.system('cls')
 
-from platform import python_version
-ver = python_version()
+ d1 = []    # declaring new list
+ b1 = []    # declaring new list
+ a1 = []    # declaring new list
+ c1 = []    # declaring new list
+ b2 = []    # declaring list for difference 
+ a2 = []    # declaring list for difference
+ c2 = []    # declaring list for difference
+ octants = []   # declaring list for octant storing
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+ work_book=load_workbook("input_octant_longest_subsequence_with_range.xlsx")
+ spreadsheet=work_book["Sheet1"] 
+ num_1=spreadsheet.max_row
+ for i in range(2,num_1+1):
+  d1.append(spreadsheet.cell(row=i,column=1).value)
+  a1.append(spreadsheet.cell(row=i,column=2).value)
+  b1.append(spreadsheet.cell(row=i,column=3).value)
+  c1.append(spreadsheet.cell(row=i,column=4).value)
 
+ b_mean_value=np.mean(b1, dtype=np.float64)     # mean of b1
+ a_mean_value=np.mean(a1, dtype=np.float64)     # mean of a1
+ c_mean_value=np.mean(c1, dtype=np.float64)     # mean of c1
 
-octant_longest_subsequence_count_with_range()
+    #appending the differences..............
+ for i in range(2,num_1+1):
+  a2.append(float(spreadsheet.cell(row=i,column=2).value)-a_mean_value) 
+  b2.append(float(spreadsheet.cell(row=i,column=3).value)-b_mean_value)
+  c2.append(float(spreadsheet.cell(row=i,column=4).value)-c_mean_value) 
 
+    #Finding the octant with the help of for loop
+ for i in range(0,num_1-1):
+  if ((a2[i]>=0) and (b2[i]>=0) and (c2[i]>=0 )):
+   octants.append(1)
+  elif((a2[i]>=0) and (b2[i]>=0) and (c2[i]<0 )):
+   octants.append(-1)
+  elif((a2[i]<0) and (b2[i]>=0) and (c2[i]>=0 )):
+   octants.append(2)
+  elif((a2[i]<0) and (b2[i]>=0) and (c2[i]<0 )):
+   octants.append(-2)
+  elif((a2[i]>=0) and (b2[i]<0) and (c2[i]>=0 )):
+   octants.append(4)
+  elif((a2[i]>=0) and (b2[i]<0) and (c2[i]<0 )):
+   octants.append(-4)    
+  elif((a2[i]<0) and (b2[i]<0) and (c2[i]>=0 )):
+   octants.append(3)
+  else:
+   octants.append(-3)
 
 
 
