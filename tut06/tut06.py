db51a1e62ddea570583e6c2c9aa2188d046a8059
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-11-20 13:46:55 +0530

    Update tut06.py

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -23,7 +23,28 @@ def attendance_report():
  lec_dates =["28/07","01/08","04/08","08/08","11/08","15/08","18/08","22/08","25/08","29/08","01/09","05/09","08/09","12/09","15/09","26/09","29/09"]
  num_of_days=len(lec_dates)
 
+ with open('input_attendance.csv', 'r') as f:
+  reader = csv.reader(f) 
 
+  all_data=[] #stores all data of all the students for all dates
+  sinle_data=[] #stores all data of particular student of all dates
+  part_date_single_data=[]  #stores all data of particular student on particular date
+  for x in roll_numbers:
+   sinle_data=[]
+   for j in lec_dates:
+    part_date_single_data=[]
+    ct1=0
+    ct2=0
+    ct3=0
+    with open('input_attendance.csv', 'r') as f:
+     reader = csv.reader(f)
+     for row in reader:
+      if j==row[0][0:5] and row[1][0:8]==x and (row[0][11:13]=="14" or row[0][11:16]=="15:00" ) and ct1==0 : 
+       ct1=ct1+1
+      elif j==row[0][0:5] and row[1][0:8]==x and (row[0][11:13]=="14" or row[0][11:16]=="15:00") and ct1>0 : 
+       ct2=ct2+1
+      elif j==row[0][0:5] and row[1][0:8]==x :
+       ct3=ct3+1
 
 attendance_report()
 
