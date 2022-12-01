5602fbcef77b15da98529e683770fffb009da2c2
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2022-12-01 16:56:19 +0530

    project2 updates.....

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -600,3 +600,323 @@ def octant_analysis_mutiple(mod,path):
 				opp_dict_mapping[2*i+1-1]=i+1
 				opp_dict_mapping[2*(i+1)-1]=-(i+1)
 
+			# this function is used to find rank of each octant
+			def rankListFind(h_c):                                     
+				temp_head_comp=h_c.copy()
+				temp_head_comp.sort(reverse=True)
+				res=[]
+
+				for i in h_c:
+					for j in range(8):
+						if(i==temp_head_comp[j]):
+							res.append(j+1)
+							break
+				# Returning the ranked list		
+				return res                                                  
+			
+			# The given list is for finding the rank 1 octants
+			def find_1st_rank(h_c):                                         
+				for i in range(8):
+					if(h_c[i]==1):
+						return opp_dict_mapping[i]
+
+			# Finding the count of rank 1 in the rank 1 mod values of octant x
+			def Rank1Count(h_c,x):                                         
+				ct=0
+				for i in h_c:
+					if(x==i):
+						ct+=1
+
+				# Return the count		
+				return ct                                                  
+			
+			 # Storing rank list for different mod values in matrix
+			mtrix_rank=[] 
+
+			# storing the octs having rank 1 in diff. mod ranges and overall                                   
+			rank1_list=[] 
+			try:                                                  
+				sheet=wb.active
+			except:
+				print("sheet activation error")	
+
+			# Adding string 'user input' at specified place
+			sheet['M4']='Mod '+str(mod)                                           
+			# For storing oct within ranges as 2-d matrix.
+			oct_range_matrix=[]   
+			# Storing elements of 9 columns in a list                                                       
+			col_list=[0]*9                                                     
+
+			 # Storing header list in 'col_list' list
+			col_list[0]='Octant ID'                                           
+
+			for i in range(0,4):
+				col_list[2*i+1]=(i+1)
+				col_list[2*(i+1)]=-(i+1)
+
+			# Appending header list in matrix	
+			oct_range_matrix.append(col_list)          
+
+			# Appending header list in worksheet                                  
+			for i in range(13,22):                                          
+				sheet.cell(row=3,column=i+1).value=col_list[i-13]
+				sheet.cell(row=3,column=i+1).border=bor
+				if(i>13):
+					sheet.cell(row=3,column=i+9).value='Rank Octant '+str(col_list[i-13])
+					sheet.cell(row=3,column=i+9).border=bor
+			sheet.cell(row=3,column=31).value='Rank1 Octant ID'
+			sheet.cell(row=3,column=32).value='Rank1 Octant Name'
+			sheet.cell(row=3,column=31).border=bor
+			sheet.cell(row=3,column=32).border=bor
+
+			# Changing values to default in list 'col_list'
+			col_list=[0]*9                                                     
+
+			# Counting total count of values in different octs
+			for i in octant:                                                
+				if(i==1):
+					col_list[1]=col_list[1]+1
+				elif(i==-1):
+					col_list[2]=col_list[2]+1
+				elif(i==2):
+					col_list[3]=col_list[3]+1
+				elif(i==-2):
+					col_list[4]=col_list[4]+1
+				elif(i==3):
+					col_list[5]=col_list[5]+1
+				elif(i==-3):
+					col_list[6]=col_list[6]+1
+				elif(i==4):
+					col_list[7]=col_list[7]+1
+				elif(i==-4):
+					col_list[8]=col_list[8]+1
+
+			CodeYelow = "00FFFF00"
+
+			# Creating overall count row
+			col_list[0]='Overall Count'                                        
+			oct_range_matrix.append(col_list)          
+
+
+			 # Appending overall count in worksheet                                 
+			for i in range(13,22):                                         
+				sheet.cell(row=4,column=i+1).value=col_list[i-13]
+				sheet.cell(row=4,column=i+1).border=bor
+			# Removing the header from list	
+			col_list.pop(0)                     
+
+
+			# Computing the rank list                                
+			rankk=rankListFind(col_list)             
+
+			 # Finding the rank 1 octant and appending in rank1_list                      
+			rank1_list.append(find_1st_rank(rankk))                         
+			mtrix_rank.append(rankk)            
+
+			# Appending overall count in worksheet                           
+			for i in range(8):                                              
+				sheet.cell(row=4,column=23+i).value=mtrix_rank[0][i]
+				sheet.cell(row=4,column=23+i).border=bor
+				if(mtrix_rank[0][i]==1):
+					sheet.cell(row=4,column=23+i).fill=PatternFill(start_color=CodeYelow,end_color=CodeYelow,fill_type="solid")
+			sheet.cell(row=4,column=31).value=rank1_list[0]
+			sheet.cell(row=4,column=32).value=octant_name_id_mapping[str(rank1_list[0])]
+			sheet.cell(row=4,column=31).border=bor
+			sheet.cell(row=4,column=32).border=bor
+									
+			
+			# Calculating the number of points given in the input
+			m=len(octant)     
+
+			# Changing the values to default in the list 'count'                                              
+			col_list=[0]*9          
+
+			# To keep track of the current index. 
+			b=0                     
+
+			# To keep track of row in worksheet                                         
+			j=4               
+
+			 # Counting number of values in different octs in mod range                                              
+			for i in octant:                                               
+				if(i==1):
+					col_list[1]=col_list[1]+1
+				elif(i==-1):
+					col_list[2]=col_list[2]+1
+				elif(i==2):
+					col_list[3]=col_list[3]+1
+				elif(i==-2):
+					col_list[4]=col_list[4]+1
+				elif(i==3):
+					col_list[5]=col_list[5]+1
+				elif(i==-3):
+					col_list[6]=col_list[6]+1
+				elif(i==4):
+					col_list[7]=col_list[7]+1
+				elif(i==-4):
+					col_list[8]=col_list[8]+1
+				b=b+1                                                     
+				if(b%mod==1):                                              
+					col_list[0]=str(b-1)+'-'                       
+				elif(b%mod==0 or b==m):
+					col_list[0]=col_list[0]+str(b-1)    
+
+					# Adding the columns of rank, rank1 and octant_name in the worksheet
+					for i in range(13,22):                                
+						sheet.cell(row=j+1,column=i+1).value=col_list[i-13]
+						sheet.cell(row=j+1,column=i+1).border=bor
+					col_list.pop(0)                                         
+					rankk=rankListFind(col_list)                           
+					rank1_list.append(find_1st_rank(rankk))                
+					mtrix_rank.append(rankk)                            
+					
+					# Adding columns of rank, rank1 and octant_name in the worksheet
+					for i in range(8):                                              
+						sheet.cell(row=j+1,column=23+i).value=mtrix_rank[j-3][i]
+						sheet.cell(row=j+1,column=23+i).border=bor
+						if(mtrix_rank[j-3][i]==1):
+							sheet.cell(row=j+1,column=23+i).fill=PatternFill(start_color=CodeYelow,end_color=CodeYelow,fill_type="solid")
+					
+					sheet.cell(row=j+1,column=31).value=rank1_list[j-3]
+					sheet.cell(row=j+1,column=32).value=octant_name_id_mapping[str(rank1_list[j-3])]
+					sheet.cell(row=j+1,column=31).border=bor
+					sheet.cell(row=j+1,column=32).border=bor
+					
+					j=j+1
+					oct_range_matrix.append(col_list)
+					col_list=[0]*9                                
+													
+			rank1_list.pop(0)                                         
+			j+=4
+			sheet.cell(row=j,column=29).value='Octant ID'        
+			sheet.cell(row=j,column=30).value='Octant Name'
+			sheet.cell(row=j,column=31).value='Count of Rank 1 Mod Values'
+			sheet.cell(row=j,column=29).border=bor                 
+			sheet.cell(row=j,column=30).border=bor
+			sheet.cell(row=j,column=31).border=bor
+			
+			# Appending the table of count of rank1 mod values
+			for i in range(8):                              
+				sheet.cell(row=j+1+i,column=29).value=opp_dict_mapping[i]
+				sheet.cell(row=j+1+i,column=30).value=octant_name_id_mapping[str(opp_dict_mapping[i])]
+				sheet.cell(row=j+1+i,column=31).value=Rank1Count(rank1_list,opp_dict_mapping[i])
+				sheet.cell(row=j+1+i,column=29).border=bor
+				sheet.cell(row=j+1+i,column=30).border=bor
+				sheet.cell(row=j+1+i,column=31).border=bor
+		
+		def oct_long_subseq_count_with_rang():
+			
+			# Header list
+			r=['Count','Longest Subsequence Length','Count']
+
+			# Appending the header of table to worksheet        
+			for i in range(3):                                     
+				sheet.cell(row=3,column=45+i).value=r[i] 
+				sheet.cell(row=3,column=45+i).border=bor    
+
+
+			octs=[]
+
+			# Appending octants on leftmost column of the table
+			for i in range(2,10,2):                                    
+				sheet.cell(row=i+2,column=45).value=i//2
+				octs.append(i//2)
+				sheet.cell(row=i+3,column=45).value=-(i//2) 
+				octs.append(-i//2)
+				sheet.cell(row=i+2,column=45).border=bor
+				sheet.cell(row=i+3,column=45).border=bor                                
+			
+			# Dictionary for mapping 
+			dict_mapping={}                                              
+			for i in range(0,4):                                            
+				dict_mapping[i+1]=2*i+1-1
+				dict_mapping[-(i+1)]=2*(i+1)-1
+
+			# List for storing number of long subsequence
+			col_list=[0]*8                                             
+			longest_length=[0]*8                                    
+			back=octant[0]
+
+			# Length of current oct
+			l=1                                                           
+			n=len(octant)
+
+			 # Storing range in temporary variable....
+			max_temp=[0]                                                  
+			ranges= [[] for x in repeat(None, 8)]            
+
+			 # Finding the length and number of longest subsequence
+			for i in range(1,n+1):  
+				  # Processing the whole if last is reached                
+				if(i==n):                                        
+					if(longest_length[dict_mapping[back]]<l):                        
+						longest_length[dict_mapping[back]]=l
+						col_list[dict_mapping[back]]=1
+
+						# Adding ending range in temp....
+						max_temp.append(df['T'][i-1])            
+						ranges[dict_mapping[back]].clear()         
+						ranges[dict_mapping[back]].append(max_temp)           
+					elif(longest_length[dict_mapping[back]]==l):
+						col_list[dict_mapping[back]]+=1
+						max_temp.append(df['T'][i-1])
+						ranges[dict_mapping[back]].append(max_temp) 
+				 # Increasing the current length by 1 if prev and current values are same....		               
+				elif(back==octant[i]):                                      
+					l+=1
+
+				# Else process the previous octant values and start with new octant
+				else:                                                      
+					if(longest_length[dict_mapping[back]]<l):
+						longest_length[dict_mapping[back]]=l
+						col_list[dict_mapping[back]]=1
+						ranges[dict_mapping[back]].clear()               
+						max_temp.append(df['T'][i-1])        
+						ranges[dict_mapping[back]].append(max_temp)                 
+					elif(longest_length[dict_mapping[back]]==l):
+						col_list[dict_mapping[back]]+=1
+						max_temp.append(df['T'][i-1])
+						ranges[dict_mapping[back]].append(max_temp)             
+					max_temp=[df['T'][i]]                     
+					l=1
+					back=octant[i]                            
+
+			# Appending the length and number of longest subsequence in table
+			for i in range(2,10):                                    
+				sheet.cell(row=i+2,column=46).value=longest_length[i-2]
+				sheet.cell(row=i+2,column=47).value=col_list[i-2]
+				sheet.cell(row=i+2,column=46).border=bor
+				sheet.cell(row=i+2,column=47).border=bor
+			b=2                                                        
+			sheet.cell(row=b+1,column=49).value='Octant ###'                  
+			sheet.cell(row=b+1,column=50).value='Longest Subsequence Length'
+			sheet.cell(row=b+1,column=51).value='Count'
+			sheet.cell(row=b+1,column=49).border=bor
+			sheet.cell(row=b+1,column=50).border=bor
+			sheet.cell(row=b+1,column=51).border=bor
+			
+			b+=2
+			for i in range(8):
+				sheet.cell(row=b,column=49).value=octs[i]            
+				sheet.cell(row=b,column=50).value=longest_length[i]
+				sheet.cell(row=b,column=51).value=col_list[i]
+				sheet.cell(row=b+1,column=49).value='Time'             
+				sheet.cell(row=b+1,column=50).value='From'
+				sheet.cell(row=b+1,column=51).value='To'
+				sheet.cell(row=b,column=49).border=bor      
+				sheet.cell(row=b,column=50).border=bor
+				sheet.cell(row=b,column=51).border=bor
+				sheet.cell(row=b+1,column=49).border=bor
+				sheet.cell(row=b+1,column=50).border=bor
+				sheet.cell(row=b+1,column=51).border=bor
+				x=ranges[i]
+				b+=2
+				for j in x:
+					# Writing ranges in worksheet
+					sheet.cell(row=b,column=50).value=j[0]     
+					sheet.cell(row=b,column=51).value=j[1]
+					sheet.cell(row=b,column=49).border=bor  
+					sheet.cell(row=b,column=50).border=bor
+					sheet.cell(row=b,column=51).border=bor
+					b+=1
+			
